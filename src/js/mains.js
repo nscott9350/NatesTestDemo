e16c396f2efd8767126e9e0d5a9522d0aa5248e4
unknown <nnat@IP-C0A84B1D.corp.amazonworkspaces.com>
2016-04-05 18:24:26 -0400

    hello world test 2

--- src/js/mains.js
+++ src/js/mains.js
@@ -2,6 +2,7 @@ $.ajax({
     type: "GET",
     url: 'http://appb.example.com/RestWebService-1/rest/hello?callback=?',
     jsonpCallback: 'callback',
+    contentType: "application/javascript",
     dataType: 'jsonp',
     success: function (data) {
         alert('Application ' + data.application + ' is ALIVE!!');
