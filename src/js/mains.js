08c1f2b71d844461ce6dc23c137ec4aa285321fa
unknown <nnat@IP-C0A84B1D.corp.amazonworkspaces.com>
2016-04-05 18:23:07 -0400

    hello world test 2

--- src/js/mains.js
+++ src/js/mains.js
@@ -1,12 +1,12 @@
 $.ajax({
-    dataType: 'JSONP',
-    jsonpCallback: 'callback',
-    url: 'http://appb.example.com/RestWebService-1/rest/hello?jsonp=callback',
     type: "GET",
+    url: 'http://appb.example.com/RestWebService-1/rest/hello?callback=?',
+    jsonpCallback: 'callback',
+    dataType: 'jsonp',
     success: function (data) {
         alert('Application ' + data.application + ' is ALIVE!!');
     },
     error: function () {
         alert("Application not found");
     }
-});
+})(jQuery);
