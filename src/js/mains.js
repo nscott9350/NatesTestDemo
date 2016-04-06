7c104c0d6dc2cbb020666909f29474a0568de0e8
unknown <nnat@IP-C0A84B1D.corp.amazonworkspaces.com>
2016-04-06 10:03:30 -0400

    hello world test 2

--- src/js/mains.js
+++ src/js/mains.js
@@ -1,9 +1,9 @@
 $.ajax({
     url: 'http://appb.example.com/RestWebService-1/rest/hello',
     type: 'GET',
-    dataType: 'json',
+    dataType: 'jsonp',
     jsonp: 'callback',
-    success: function(){
-        alert(' is ALIVE!!');
+    success: function(data){
+        alert('Application ' + data.application + ' is ALIVE!!');
     }
 });
